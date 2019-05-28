6de1d9792814b8a5dec1a92e4d49c4fdcfce4004
Rebecca Wallander <FakeShemp@users.noreply.github.com>
2019-05-28 16:53:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ import routes from "./routes";
 import { connect } from 'mongoose';
 const app = express();
 
+let allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+}
+
 connect('mongodb://localhost:27017/students', { useNewUrlParser: true });
 
 const port = process.env.PORT || 3012;
 
 app.use(json());
 app.use(urlencoded({ extended: true }))
+app.use(allowCrossDomain)
 app.use('/', routes);
 
 app.listen(port, () => {
